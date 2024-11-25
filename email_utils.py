a4021ddf8c186ca7dfb0c58f1e48eb278af14bfa
alfarojdd <alfaro.jdd@gmail.com>
2024-11-25 17:10:14 +0100

    Implementacion de grabación por tiempo

--- email_utils.py
+++ email_utils.py
@@ -1,22 +1,43 @@
+import os
+from dotenv import load_dotenv
 import smtplib
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
-def send_email(subject, body, to):
-    sender_email = "TUCORREO"
-    password = "TUCONTRASEÑA"
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
 
+def send_email(subject, body, to_email, file_path=None):
+    from_email = os.getenv('EMAIL_ADDRESS') 
+    password = os.getenv('EMAIL_PASSWORD') 
+    
+    msg = MIMEMultipart()
+    msg['From'] = from_email
+    msg['To'] = to_email
+    msg['Subject'] = subject
+    
+    # Agregar el cuerpo del correo
+    msg.attach(MIMEText(body, 'plain'))
+
+    if file_path:
+        # Adjuntar el archivo de video
+        with open(file_path, 'rb') as file:
+            part = MIMEBase('application', 'octet-stream')  # Tipo MIME genérico para archivos binarios
+            part.set_payload(file.read())
+            encoders.encode_base64(part)  # Codificación del archivo en base64 para enviarlo por email
+            part.add_header('Content-Disposition', f'attachment; filename="{file_path}"')
+            msg.attach(part)
+
+    # Configurar el servidor SMTP de Gmail
     try:
-        # Configura el mensaje
-        msg = MIMEText(body)
-        msg['Subject'] = subject
-        msg['From'] = sender_email
-        msg['To'] = to
-
-        # Conecta al servidor SMTP
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-            server.login(sender_email, password)
-            server.sendmail(sender_email, to, msg.as_string())
-
-        print("Correo enviado!")
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(from_email, password)
+        text = msg.as_string()
+        server.sendmail(from_email, to_email, text)
+        server.quit()
+        print("Correo enviado con éxito")
     except Exception as e:
         print(f"Error enviando correo: {e}")
--- motion_detector.py
+++ motion_detector.py
@@ -2,64 +2,88 @@ import cv2
 import numpy as np
 from email_utils import send_email
 import time
+import os
 
 def detect_motion_with_alerts():
-    time.sleep(10)
-    cap = cv2.VideoCapture(0)  # Abre la cámara
+    cap = cv2.VideoCapture(0)
 
     if not cap.isOpened():
         print("Error al abrir la cámara")
         return
 
-    # Espera un poco para estabilizar la cámara antes de comenzar a capturar
     print("Esperando a que la cámara se estabilice...")
-    time.sleep(2)  # Espera 2 segundos
+    time.sleep(2)
 
-    # Captura los primeros dos cuadros para la comparación
     _, frame1 = cap.read()
     _, frame2 = cap.read()
 
-    alert_sent = False  # Para evitar múltiples alertas
+    motion_detected = False
+    recording = False
+
+    max_recording_time = 1
+    thresh_recording_time = 1
+    recording_time = 0
 
     while cap.isOpened():
-        # Calcula la diferencia entre los dos cuadros
+        
+        #Detección del movimiento
+        
         diff = cv2.absdiff(frame1, frame2)
         gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-        blur = cv2.GaussianBlur(gray, (5, 5), 0)
-        
-        # Aplica un umbral para detectar áreas de movimiento
+        blur = cv2.GaussianBlur(gray, (15, 15), 0)
         _, thresh = cv2.threshold(blur, 25, 255, cv2.THRESH_BINARY)
-        dilated = cv2.dilate(thresh, None, iterations=3)
-
-        # Encuentra los contornos de las áreas con movimiento
+        dilated = cv2.dilate(thresh, None, iterations=20)
         contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        motion_detected = False
         for contour in contours:
-            if cv2.contourArea(contour) < 500:  # Ignorar áreas pequeñas que no son movimiento significativo
-                continue
-            motion_detected = True
-            x, y, w, h = cv2.boundingRect(contour)
-            cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            if cv2.contourArea(contour) >= 500:  
+                motion_detected = True
+                x, y, w, h = cv2.boundingRect(contour)
+                cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-        # Si se detecta movimiento y no se ha enviado una alerta
-        if motion_detected and not alert_sent:
-            send_email("Movimiento Detectado", "Se ha detectado movimiento en tu webcam", "TUCORREO")
-            alert_sent = True  # Evitar enviar múltiples alertas
-
-        # Muestra el cuadro en la ventana
         cv2.imshow("Motion Detector", frame1)
 
-        # Actualiza los cuadros
+        #Crear funcion para grabar a la vez que se hace el bucle para comprobar si hay movimiento
+
+        if motion_detected and not recording:
+            video_path = "motion_video.mp4"
+            fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+            out = cv2.VideoWriter(video_path, fourcc, 20.0, (640, 480))  # Graba con resolución de 640x480 y 20 fps
+            start_time = time.time()
+            recording_time = 0
+            recording = True
+            print(f'Iniciando grabación')
+        elif motion_detected:
+            recording_time = recording_time + time.time() - start_time
+            start_time = time.time()
+
+        if recording:
+            if time.time() - start_time < thresh_recording_time and recording_time <= max_recording_time:
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                out.write(frame)
+                cv2.imshow("Recording Video", frame)  # Ocultar despues
+            else:
+                out.release() 
+                cv2.destroyWindow("Recording Video")
+                send_email("Movimiento Detectado", "Se ha detectado movimiento en tu webcam", "TUCORREO", video_path)
+                recording = False
+                print('Fin de la grabación')
+                if os.path.exists(video_path):
+                    os.remove(video_path)
+            
+        motion_detected = False
         frame1 = frame2
         _, frame2 = cap.read()
 
-        # Verifica si la tecla 'q' es presionada para salir
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
+
     cap.release()
     cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     detect_motion_with_alerts()
